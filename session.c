16376d97a4595d3690ee175948994ce31b28ef52
Quivor33 <quim.vila01@estudiant.upf.edu>
2025-02-07 11:27:04 +0100

    Update session.c

--- session.c
+++ session.c
@@ -1,11 +1,12 @@
 #include "session.h"
 
 void restart_session_game_state(Session *session){
-    init_game_state(&session->current_game_state);
+   init_game_state(&session->current_game_state);
 }
 
 void init_session(Session *session){
-    session->best_score=0;
-    restart_session_game_state(session);
+  session->best_score = 0;
+  restart_session_game_state(session);
 }
+
 
